e09492a018f9b237259e578e7453e6237975a539
Lucute2001 <lch.ryu2001@gmail.com>
2023-04-17 02:11:29 +0700

    Add New Note Screen Design

--- app/src/main/java/com/example/notespro/MainActivity.java
+++ app/src/main/java/com/example/notespro/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton addNoteBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        addNoteBtn = findViewById(R.id.add_note_btn);
+        addNoteBtn.setOnClickListener(v->startActivity(new Intent(MainActivity.this, NoteDetailsActivity.class)));
     }
 }
